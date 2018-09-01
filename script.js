ce9d6413eacbd7067f67a34f8d2e86bcb45b7189
Unknown <bghahate265@west-mec.org>
2018-08-31 18:25:40 -0700

    Second Commit
    Started Documentation

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 /*  Project 01_11_02
 
-    Author: 
-    Date:   
+    Author: Braddock Ghahate
+    Date: 08.31.18
 
     Filename: script.js
 */
 
 "use strict";
+
+//global variables
+var httpRequest = false; // var to hold XMR object
+var entry = "^IXIC";
+
+// function to create an XMR
